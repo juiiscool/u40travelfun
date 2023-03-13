12660e179f2520ae28d4d21338087331d4ac4c1c
Joana <126071300+juiiscool@users.noreply.github.com>
2023-03-13 01:23:32 +0100

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,9 @@
+$("#pMbuton").first().click(function myFunction() {
+    document.getElementById("carouselExampleCaptions").style.backgroundImage = "url(images/party.gif)";
+ });
+
+
+ function myFunction(alert) {
+    alert("Mesazhi u dergua.\nDo ju njoftojme se shpejti!");
+  };
+
